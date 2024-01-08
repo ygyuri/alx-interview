b0d593fbd91f650806787cf961bb63c32c6be7b8
YURI GIDEON <gideonyuri15@gmail.com>
2024-01-08 17:56:36 +0300

    0x01. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+lockbox challenge being worked on
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    checks all boxes and unlocks if key is available
+    """
+    return "True"
